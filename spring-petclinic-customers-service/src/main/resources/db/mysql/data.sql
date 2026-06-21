8c1fbc92c7ad9594ab76efe790ef3c60d327b5f7
taxgaard-twoday <david.taxgaard@twoday.com>
2026-06-21 13:38:10 +0200

    Change bird type to norwegian blue in data.sql

--- spring-petclinic-customers-service/src/main/resources/db/mysql/data.sql
+++ spring-petclinic-customers-service/src/main/resources/db/mysql/data.sql
@@ -2,7 +2,7 @@ INSERT IGNORE INTO types VALUES (1, 'cat');
 INSERT IGNORE INTO types VALUES (2, 'dog');
 INSERT IGNORE INTO types VALUES (3, 'lizard');
 INSERT IGNORE INTO types VALUES (4, 'snake');
-INSERT IGNORE INTO types VALUES (5, 'bird');
+INSERT IGNORE INTO types VALUES (5, 'norwegian blue');
 INSERT IGNORE INTO types VALUES (6, 'hamster');
 
 INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
